6266b5298e72786397ccb086c3689bf4af0bd021
helenaSarrion <hsarexp808@g.educaand.es>
2023-03-19 01:04:03 +0100

    filtrado por precio done :)

--- proyecto-tienda/src/Controller/ProductosController.php
+++ proyecto-tienda/src/Controller/ProductosController.php
@@ -2,30 +2,51 @@
 
 namespace App\Controller;
 
+use App\Entity\Productos;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-use App\Entity\Productos;
-use App\Entity\Categorias;
 
 class ProductosController extends AbstractController
 {
-
     /**
      * @Route("/productos", name="productos")
      */
-    public function productos(Request $request): Response
+    public function productos(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $productos = $this->getDoctrine()->getRepository(Productos::class)->findAll();
-        foreach ($productos as $producto) {
-            dump($producto->getImageLink());
+        $repository = $entityManager->getRepository(Productos::class);
+        $queryBuilder = $repository->createQueryBuilder('p');
+
+        // Obtener los productos filtrados por precio
+        $precio = $request->query->get('precio');
+        if ($precio) {
+            switch ($precio) {
+                case '0-50':
+                    $queryBuilder->andWhere('p.precio <= 50');
+                    break;
+                case '50-100':
+                    $queryBuilder->andWhere('p.precio > 50 AND p.precio <= 100');
+                    break;
+                case '100-200':
+                    $queryBuilder->andWhere('p.precio > 100 AND p.precio <= 200');
+                    break;
+                case '200+':
+                    $queryBuilder->andWhere('p.precio > 200');
+                    break;
+            }
         }
+
+        // Obtener los productos
+        $productos = $queryBuilder->getQuery()->getResult();
 
         return $this->render('productos/index.html.twig', [
             'productos' => $productos,
         ]);
     }
+
+
 
     public function productosPorCategoria(Request $request, $categoria)
     {
@@ -36,6 +57,19 @@ class ProductosController extends AbstractController
             'productos' => $productos,
             'categoria' => $categoria,
         ]);
+    }
+
+    private function findByPrecio($min, $max)
+    {
+        return $this->getDoctrine()
+            ->getRepository(Productos::class)
+            ->createQueryBuilder('p')
+            ->andWhere('p.precio >= :min')
+            ->andWhere('p.precio <= :max')
+            ->setParameter('min', $min)
+            ->setParameter('max', $max)
+            ->getQuery()
+            ->getResult();
     }
 
 }
